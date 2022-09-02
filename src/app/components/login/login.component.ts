e7b33bf491bfe9e271846ffc5801bd4f12f9191e
emircalife <emircalife@gmail.com>
2022-09-02 13:56:36 -0300

    Validação de formulários para LOGIN

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Credenciais } from 'src/app/models/credenciais';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  creds: Credenciais = {
+    email: '',
+    senha: ''
+  }
+
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  validaCampos(): boolean{
+    return (this.email.valid && this.senha.valid);
+  }
 }
