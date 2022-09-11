8388622b06397c7d6124e1f8895c71c92190c188
emircalife <emircalife@gmail.com>
2022-09-11 13:17:01 -0300

    Última atualização CLIENTE_CREATE

--- src/app/services/tecnico.service.ts
+++ src/app/services/tecnico.service.ts
@@ -11,7 +11,12 @@ export class TecnicoService {
 
   constructor(private http: HttpClient) { }
 
-  findAll(): Observable<Tecnico[]> {
+  findAll(): Observable<Tecnico[]>{
     return this.http.get<Tecnico[]>(`${API_CONFIG.baseUrl}/tecnicos`);
   }
+
+  create(tecnico: Tecnico): Observable<Tecnico> {
+    return this.http.post<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos`, tecnico);
+  }
+    
 }
